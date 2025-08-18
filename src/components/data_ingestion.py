02182ec99ad1a89e380865017c06208d23989466
Jnyambok <juliusnyambok14@gmail.com>
2025-08-18 19:17:28 +0300

    (Feature) : Data Ingestion Module Completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -15,6 +15,7 @@ class DataIngestionConfig:
     project_root = Path(__file__).resolve().parent.parent.parent  # jnyambok-soca-scores/
     data_dir = project_root / "datasets" 
     common_data_dir = data_dir / "common_data"
+    ingested_dir = data_dir / "ingested_data"
     csv_file = common_data_dir / "english_league_data_urls.csv"
 
 # ===== DATA SOURCE IDENTIFICATION =====#
@@ -39,7 +40,7 @@ class DataIngestion:
 
 # ===== APPENDING METADATA - ADDING COMPETITION NAME AND SEASON ID =====#
     def add_metadata(self,urls):
-        print("Adding metadata to urls")
+        print("Adding metadata to urls.........")
         logging.info("Entering the adding metadata microcomponent")
         dataframes=[]
         for index,row in urls.iterrows():
@@ -61,12 +62,48 @@ class DataIngestion:
                 raise CustomException(e,sys) from e
         print("Step 2: Metadata successfully appended!")
         return dataframes
+    
+# ===== COMBINING ALL THE DATASETS =====#
+    def combine_dataframes(self,dataframes):
+        try:
+            print("Combining all the datasets.......")
+            final_df = pd.concat(dataframes,ignore_index=True)
+            final_df = final_df.drop('Div',axis=1)
+            logging.info("Datasets successfully ingested and concatenated!")
+            logging.info(f"Final dataset shape : {final_df.shape}")
+            #Validating the final dataset with metadata
+            display_columns = ['season_id','competition_name','Date','HomeTeam','AwayTeam','FTHG','FTAG','FTR']
+            available_columns = [ col for col in display_columns if col in final_df.columns]
+            logging.info(final_df[available_columns].sample(10).to_string(index=False))
+        except Exception as e:
+            raise CustomException(e,sys) from e
+        print("Step 3 : Datasets successfully concatenated!")
+        return final_df
+    
+
+
+    def save_datasets(self,final_df):
+        print("Saving the concatenated datasets.......")
+        try:
+            output_dir = self.ingestion_config.ingested_dir
+            output_dir.parent.mkdir(exist_ok=True)
+            output_file = output_dir/ "enhanced_dataset.csv"
+
+            final_df.to_csv(output_file,index=False)
+            logging.info(f"Enhanced Dataset saved to: {output_file}")
+        except Exception as e:
+            raise CustomException(e,sys) from e
+        print("Step 4: Enhanced dataset saved to ingested folder!")
+
 
 
 
 if __name__ == "__main__":
     obj = DataIngestion()
     urls = obj.load_csv()
-    obj.add_metadata(urls)
+    dataframes = obj.add_metadata(urls)
+    final_df=obj.combine_dataframes(dataframes)
+    obj.save_datasets(final_df)
+
 
 
