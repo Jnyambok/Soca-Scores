baee5fbd34b39db233a124b824daf9a675c369b6
Jnyambok <juliusnyambok14@gmail.com>
2025-08-18 18:35:44 +0300

    (feature): Data Ingestion: Addition of metadata information

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -17,26 +17,56 @@ class DataIngestionConfig:
     common_data_dir = data_dir / "common_data"
     csv_file = common_data_dir / "english_league_data_urls.csv"
 
+# ===== DATA SOURCE IDENTIFICATION =====#
 class DataIngestion:
     def __init__(self):
         self.ingestion_config = DataIngestionConfig()
     
     def load_csv(self):
-        logging.info("Entered the data ingestion component")
+        print("Identifying the data source...............")
+        logging.info("Entered the loading csv microcomponent")
         try:
             if self.ingestion_config.csv_file.exists():
-                logging.info(f"✅ Found file: {self.ingestion_config.csv_file}")
+                logging.info(f"Found file: {self.ingestion_config.csv_file}")
                 urls = pd.read_csv(self.ingestion_config.csv_file)
-                logging.info(f'📊File has been loaded successfully. The url dataset has {urls.shape[0]} rows and {urls.shape[1]} columns')
+                logging.info(f'File has been loaded successfully. The url dataset has {urls.shape[0]} rows and {urls.shape[1]} columns')
             else:
-                logging.info(f"❌ File not found: {self.ingestion_config.csv_file}")
+                logging.info(f"File not found: {self.ingestion_config.csv_file}")
         except Exception as e:
             raise CustomException(e, sys) from e
-        print("Data Source has been identified")
+        print("Step 1 complete: Data Source has been identified!")
+        return urls
+
+# ===== APPENDING METADATA - ADDING COMPETITION NAME AND SEASON ID =====#
+    def add_metadata(self,urls):
+        print("Adding metadata to urls")
+        logging.info("Entering the adding metadata microcomponent")
+        dataframes=[]
+        for index,row in urls.iterrows():
+            season_id = row["Season_ID"]
+            seasons_url = row["Seasons_url"]
+            competition_name = row["Competition_name"]
+            logging.info(f"\n Processing: {season_id} - {competition_name}")
+            logging.info(f"URL : {seasons_url}")
+            # === Metadata collected ===#
+
+            try:
+                epl_data = pd.read_csv(seasons_url)
+                epl_data["season_id"] = season_id
+                epl_data["competition_name"] = competition_name
+                dataframes.append(epl_data)
+                logging.info(f"Success: {epl_data.shape[0]} matches loaded")
+                logging.info(f" Columns: {epl_data.shape[1]} (including metadata)")
+            except Exception as e:
+                raise CustomException(e,sys) from e
+        print("Step 2: Metadata successfully appended!")
+        return dataframes
+
 
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.load_csv()
+    urls = obj.load_csv()
+    obj.add_metadata(urls)
 
 
